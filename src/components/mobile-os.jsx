47e17ef70af4d07807b7e99244efdf6a9b30a977
Ganesh Aher <aherganesh194@gmail.com>
2022-05-18 09:43:15 +0530

    final

---
+++ src/components/mobile-os.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const Mob = () => {
+return <div> 
+<h1>  Mobile Operating System
+</h1>
+<ul>
+<li>
+Android
+</li>
+
+<li>
+Blackberry
+</li>
+
+<li>
+Iphone
+</li>
+
+<li>
+Windows Phone
+</li>
+
+</ul>
+
+
+
+
+
+<h1>  Mobile Manufactures
+</h1>
+
+<div> 
+    <ul> 
+    <li>
+Samsungs
+</li>
+
+<li>
+HTC
+</li>
+
+<li>
+Micromax
+</li>
+
+<li id='white'>
+Apple
+</li>
+</ul>
+</div>
+
+    </div>
+};
+
+export default Mob;
